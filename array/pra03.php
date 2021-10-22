8dacc00bed96c472fc3cec871da91f93559fb0f3
zhihau <shiusoft@gmail.com>
2021-10-22 14:52:41 +0800

    外觀優化

--- array/pra03.php
+++ array/pra03.php
@@ -8,6 +8,16 @@
 
 </title>
 <link rel="stylesheet" href="./css/style.css">
+<style>
+    .ball{
+        background:lightgreen;
+        border-radius:50%;
+        padding:16px;
+        display:inline-block;
+        margin:5px;
+        width:16px;
+    }
+</style>
 </head>
 <body>
     <h1>威力彩電腦選號沒有重覆號碼</h1>
@@ -21,10 +31,14 @@
         }
         $i++;
     }
-    echo "<pre>";
-    print_r($nums);
-    echo "</pre>";
+    // echo "<pre>";
+    // print_r($nums);
+    // echo "</pre>";
     echo "迴圈次數:".$i;
+    echo "<hr>";
+    foreach($nums as $num){
+        echo "<div class='ball'>".$num."</div> ";
+    }
     ?>
 </body>
 </html>
