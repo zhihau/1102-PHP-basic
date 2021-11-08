7a73793e5156b9f423cf3bdd06ddb10cc2c63c0f
zhihau <shiusoft@gmail.com>
2021-11-08 09:18:05 +0800

    coookie示範

---
+++ session/cookie.php
@@ -0,0 +1,6 @@
+<h1>設定cookie變數</h1>
+<div>變數名name, 值shiu, 存活時間60秒</div>
+<?php
+setcookie("name","mack",time()+60);
+
+?>
---
+++ session/get_cookie.php
@@ -0,0 +1,6 @@
+<?php
+session_start();
+echo '帳號是:'.$_COOKIE['name'];
+
+echo '<hr>';
+?>
