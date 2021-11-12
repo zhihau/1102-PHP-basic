0b111ce07196035e76759393d3d594e3e23d369c
zhihau <shiusoft@gmail.com>
2021-11-12 16:29:31 +0800

    帶入get參數到sql語句中增加查詢結果變化

--- db/connect.php
+++ db/connect.php
@@ -2,7 +2,11 @@
 $dsn="mysql:host=localhost;charset=utf8;dbname=students";
 // $dbuser='root'
 $pdo=new PDO($dsn,'root','');
-$sql="select * from `students` where `id`<30";
+
+
+$range=(isset($_GET['id']))?$_GET['id']:20;//葉面呈現動態
+
+$sql="select * from `students` where `id`< $range";
 // $sql="select `name`,`secondary` from `students` where `secondary` like '%安樂國中%' and `name` like '陳%'";
 $rows=$pdo->query($sql)->fetchAll();
 // $pdo=new PDO('資料庫的連線資訊','帳號','密碼','[其他參數]');
@@ -32,6 +36,7 @@ $rows=$pdo->query($sql)->fetchAll();
         <td>學號</td>
         <td>座位</td>
         <td>姓名</td>
+        <td>性別</td>
         <td>出生年月日</td>
         <td>畢業國中</td>
     </tr>
@@ -41,9 +46,17 @@ foreach($rows as $row){
     echo  "<td>" . $row['uni_id'] . "</td>";
     echo  "<td>" . $row['seat_num'] . "</td>";
     echo  "<td>" . $row['name'] . "</td>";
+    if(mb_substr($row['national_id'],1,1)==1){
+        echo "<td>男生</td>";
+    }else{
+        echo "<td>女生</td>";
+
+    }
     echo  "<td>" . $row['birthday'] . "</td>";
     echo "<td>" .  $row['secondary'] . "</td>";
     echo "</tr>";
 }
 ?>
 </table>
+<!-- 表單給予不同的參數，得到不同結果
+?id=50 -->
