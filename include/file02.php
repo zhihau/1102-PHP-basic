7559bfdca53b75447ae042dc306441d2348a4077
zhihau <shiusoft@gmail.com>
2021-11-15 09:29:17 +0800

    測試include功能

---
+++ include/file02.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test include and require</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php include "header.html";?>
+    <nav>
+        <a href="file01.php">file01.php</a>
+        <a href="file02.php">file02.php</a>
+        <a href="file03.php">file03.php</a>
+    </nav>
+    <div class="body">
+        file02的內容
+    </div>
+    <footer>
+        110 php資料庫網頁設計班
+    </footer>
+</body>
+</html>
