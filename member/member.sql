2b57903a14e764a0d5acd24534b96e9d6d1374d6
zhihau <shiusoft@gmail.com>
2021-11-15 14:10:18 +0800

    完成註冊用表單

---
+++ member/member.sql
@@ -0,0 +1,88 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2021-11-15 06:43:24
+-- 伺服器版本： 10.4.21-MariaDB
+-- PHP 版本： 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫: `member`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `account`
+--
+
+CREATE TABLE `account` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `account` varchar(32) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `password` varchar(32) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `mail` varchar(32) COLLATE utf8mb4_unicode_520_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `member`
+--
+
+CREATE TABLE `member` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(16) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `birthday` date NOT NULL,
+  `address` varchar(64) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+  `mobile` varchar(14) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+  `level` tinyint(2) UNSIGNED NOT NULL DEFAULT 0,
+  `create_time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `account`
+--
+ALTER TABLE `account`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `member`
+--
+ALTER TABLE `member`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- member/reg.php
+++ member/reg.php
@@ -16,7 +16,39 @@
     <?php include "./include/nav.php";?>
     <?php include "./include/side_bar.php";?>
     <div class="content">
-reg
+<form action="./api/reg.php" method="post" id="regForm">
+    <table>
+        <tr>
+            <td>帳號:</td>
+            <td><input type="text" name="account" id=""></td>
+        </tr>
+        <tr>
+            <td>密碼:</td>
+            <td><input type="text" name="password" id=""></td>
+        </tr>
+        <tr>
+            <td>電子郵件:</td>
+            <td><input type="text" name="mail" id=""></td>
+        </tr>
+        <tr>
+            <td>姓名:</td>
+            <td><input type="text" name="name" id=""></td>
+        </tr>
+        <tr>
+            <td>電話:</td>
+            <td><input type="text" name="mobile" id=""></td>
+        </tr>
+        <tr>
+            <td>地址:</td>
+            <td><input type="text" name="address" id=""></td>
+        </tr>
+        <tr>
+            <td>生日:</td>
+            <td><input type="date" name="birthday" id=""></td>
+        </tr>
+    </table>
+    <div><input type="submit" value="確認送出"></div>
+</form>
     </div>
     <?php include "./include/footer.php";?>
     
